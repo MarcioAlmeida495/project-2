e92a704e932ea734c6b9f5b8a115d34396d3a5c8
Marcio Almeida <marcioalmeida13495@gmail.com>
2024-09-27 07:22:53 -0300

    project-2

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import { ButtonReverse } from './Components/Button/ButtonReverse';
+import { useState } from 'react';
+
+function App() {
+  const [reverse, setReverse] = useState(false);
+  const [counter, setCounter] = useState(0);
+  const reverseClass = reverse ? 'reverse' : '';
+  const handleClick = () => {
+    setReverse(!reverse);
+  };
+  const handleClickCounter = () => {
+    setCounter(counter + 1);
+  };
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className={`App-logo ${reverseClass}`} alt="logo" />
+        <h1>{`Contador: ${counter}`}</h1>
+        <ButtonReverse onClick={handleClick} text="Click To Reverse Spin" />
+        <ButtonReverse onClick={handleClickCounter} text="Click To Increment" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Button/ButtonReverse.jsx
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+import { PropTypes } from 'prop-types';
+export class ButtonReverse extends Component {
+  render() {
+    const { text, onClick } = this.props;
+    return <button onClick={onClick}>{text}</button>;
+  }
+}
+
+ButtonReverse.propTypes = {
+  text: PropTypes.string,
+  onClick: PropTypes.number,
+};
