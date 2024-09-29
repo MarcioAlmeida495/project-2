4776df42737807ef479df916efd7d8ba2914b0ca
Marcio Almeida <marcioalmeida13495@gmail.com>
2024-09-29 16:53:30 -0300

    useMemo useCallback

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,55 @@
 import logo from './logo.svg';
 import './App.css';
 import { ButtonReverse } from './Components/Button/ButtonReverse';
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback, useMemo } from 'react';
+import p from 'prop-types';
 
+const ComponentButton = React.memo(function Button ({fn, num, text}) {
+  console.log(fn);
+  return <button  onClick={() => fn(num)}>{text}</ button>
+})
+
+ComponentButton.propTypes = {
+  fn: p.func,
+  text: p.string,
+  num: p.number,
+}
+
+const ComponentMemorizado = ({fn, num, text}) => {
+  return <button  onClick={() => fn(num)}>{text}</ button>
+}
+ComponentMemorizado.propTypes = {
+  fn: p.func,
+  text: p.string,
+  num: p.number,
+}
 function App() {
-  const [counter, setCounter] = useState(0);
+  const [counter, setCounter] = useState(1);
   const [counter2, setCounter2] = useState(10);
 
-
   useEffect(() => {
-    console.log(counter2);
+    console.log(counter);
   }, [counter]);
 
   useEffect(() => {
-    document.getElementById('myH1').innerHTML = 'HELLO';
   });
+
+  const handleClick = useCallback((value)=>{
+    setCounter((counter) => counter + value);
+  }, []);
+
+  const btn = useMemo( () =>  ComponentMemorizado(handleClick, 10, "fodaci"));
 
   return (
     <div className="App">
       <h1 id="myH1">Contador: {counter}</h1>
       <h1>Contador2: {counter2}</h1>
+
+      <button onClick={() => setCounter(counter*2)}>NoIncrement</button>
       <button onClick={() => setCounter(counter + 1)}>Increment</button>
       <button onClick={() => setCounter2(counter2 + 2)}>Increment</button>
+      <ComponentButton fn={handleClick} num={10} text="Mazio" />
+      {useMemo(() => <ComponentButton fn={handleClick} num={10} text="Mazio" />)}
     </div>
   );
 }
