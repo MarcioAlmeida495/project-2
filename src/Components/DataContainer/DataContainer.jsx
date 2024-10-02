7aedef786ba0e450b7a82a25df078ec57b939f1e
Marcio Almeida <marcioalmeida13495@gmail.com>
2024-10-02 08:18:07 -0300

    0210240818

--- src/Components/DataContainer/DataContainer.jsx
+++ src/Components/DataContainer/DataContainer.jsx
@@ -4,8 +4,6 @@ import './styles.css';
 import PropTypes from 'prop-types';
 import { LoadingIcon } from '../Loading/LoadingIcon';
 
-var bool = true;
-
 var init = {
   method: 'POST', // Método HTTP (pode ser 'GET', 'POST', 'PUT', 'DELETE', etc.)
   headers: {
@@ -18,40 +16,47 @@ var init = {
   })
 };
 
-function DataContainer({search}) {
+function DataContainer({ search }) {
   console.log('dataContainer renderizou');
-  console.log(search)
-  var data = [];
+
+  // Estado para controlar o texto e o carregamento
   const [text, setText] = useState('Textando');
+  const [isLoading, setIsLoading] = useState(true); // Estado de carregamento
   const loading = LoadingIcon();
 
-  init.body = JSON.stringify({name: search});
-  console.log(init);
-  useEffect(()=>{
+  init.body = JSON.stringify({ name: search });
+
+  useEffect(() => {
     console.log('AQUI??!?!?!');
+    setIsLoading(true); // Inicia o carregamento
+
     fetch('http://localhost:8080/fetch', init)
-      .then(r=> r.json()
-      .then(r => {
-        data = Object.values(r);
-        var arr = data[0].split(/\r?\n/);
-        console.log(arr);
-        console.log(data)
-        if(data)bool = !bool;
-        setText(data);
-      }));
+      .then((r) => r.json())
+      .then((r) => {
+        const data = Object.values(r);
+        const arr = data[0].split(/\r?\n/);
+
+        console.log('Dados recebidos:', data);
+        setText(data); // Define os dados recebidos como texto
+        setIsLoading(false); // Termina o carregamento
+      });
   }, [search]);
 
+  useEffect(() => {
+    console.log('Texto alterado:', text);
+  }, [text]);
 
   return (
-    <div className='card'>
-      <h4 className='card-header' >{search}</h4>
-      <div className='card-context' >{bool ? loading : text}</div>
-    </ div>
-  )
+    <div className="card">
+      <h4 className="card-header">{search}</h4>
+      {/* Se estiver carregando, mostra o ícone de carregamento, senão mostra o texto */}
+      <div className="card-context">{isLoading ? loading : text}</div>
+    </div>
+  );
 }
 
 export default DataContainer;
 
 DataContainer.propTypes = {
-  search: PropTypes.string,
-}
+  search: PropTypes.string.isRequired,
+};
